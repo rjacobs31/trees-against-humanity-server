e33d72af9ffa26b14411489f6a65200fb21c8566
Rudolph Jacobs <jacobsrudolph@yahoo.com>
2018-11-24 18:05:41 +0200

    Add initial Cobra/Viper commands.

---
+++ cmd/root.go
@@ -0,0 +1,30 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/viper"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "trees-against-humanity",
+	Short: "Trees Against Humanity is an unofficial Cards Against Humanity server",
+	Run: func(cmd *cobra.Command, args []string) {
+		// Do Stuff Here
+	},
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	viper.SetEnvPrefix("trees_against")
+	viper.AutomaticEnv()
+}
